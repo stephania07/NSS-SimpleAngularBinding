aaf119e01966b6e58c7b87debc8cf84fe370df09
Stephania <stephania07>
2014-12-04 15:20:42 -0600

    new app Todos

--- js/main.js
+++ js/main.js
@@ -2,10 +2,27 @@
   'use strict';
 
 //var app = angular.module('myApp', []);
-angular.module('myApp', [])
-.controller('SimpleController', function(){
-	var vm = this;			
-  vm.name = "Stephania";
-	vm.friends = ['Ashley', 'Bob', 'Carley', 'Dave'];
-});
+angular.module('todoApp', [])
+.controller('TodoController', function(){
+	var vm = this;	
+  vm.tasks = [
+  { name: 'Learn Angular',
+		desc: 'If I could learn Angular I\'d be soooo happy!',
+	  due: 'Today'
+	},			
+  { name: 'Finish Tic Tac Toe',
+		desc: 'Firebase Arrrrggghhhhhhhh!',
+	  due: 'Monday'
+	},
+  { name: 'Get a job',
+		desc: 'profit!',
+	  due: 'April 2015'
+  }			
+	];
+  
+  vm.addNewTask = function(){
+    vm.tasks.push(vm.newTask);
+    vm.newTask = null;
+	};	
+ });
 }());
