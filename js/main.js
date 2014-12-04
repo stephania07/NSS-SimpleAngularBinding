01305443926a66f9b960b7fe4db33c66f14ee052
Stephania <stephania07>
2014-12-04 15:37:20 -0600

    Added Delet functionality

--- js/main.js
+++ js/main.js
@@ -23,6 +23,10 @@ angular.module('todoApp', [])
   vm.addNewTask = function(){
     vm.tasks.push(vm.newTask);
     vm.newTask = null;
-	};	
+	};
+ vm.removeTodo = function(todo){
+    var index = vm.tasks.indexOf(todo);
+	  vm.tasks.splice(index, 1);
+ }	
  });
 }());
